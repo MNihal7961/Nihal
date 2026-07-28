931ee534885e3dcc4357d688474b6758b1ab93aa
MNihal7961 <mnihal7961@gmail.com>
2026-07-28 15:55:54 +0530

    feat: add constants for full name, email, and social links

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const FULL_NAME = "Nihal M";
+
+export const EMAIL = "mnihal7961@gmail.com";
+
+export const SOCIAL_LINKS = {
+  linkedin: "https://www.linkedin.com/in/nihalmaliyekkal",
+  github: "https://github.com/MNihal7961",
+};
